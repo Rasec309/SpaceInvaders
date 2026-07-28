c0beec78fe9052936b3ab8f711973e45a5e1cb42
EggsF0rAll <carol.perez@uteca.edu.mx>
2026-07-27 20:29:03 -0600

    27Juli

--- Assets/Scrips/PoolManager.cs
+++ Assets/Scrips/PoolManager.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using System.Collections.Generic;
-
+ 
 public class PoolManager : MonoBehaviour
 {
+    public static PoolManager Instance {get; private set;}
+    private void Awake()
+    {
+        Instance = this;
+    }
     private Dictionary<GameObject, Pool> pools = new Dictionary<GameObject, Pool>();
     public void RegisterPrefab(GameObject prefab)
     {
@@ -25,5 +30,6 @@ public class PoolManager : MonoBehaviour
             RegisterPrefab(prefab);
             return pools[prefab].InstantiateObject(position);
         }
-    }
+ 
+       }
 }
